9bc740bed972141dfcaa56c831fd40c7ceabebfe
ElliotScher <ecscher84@gmail.com>
2024-11-13 12:01:53 -0500

    first commit

---
+++ TempHumidRTP.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class TempHumidRTP extends TempHumidTemplate {
+
+    @Override
+    public void intakeData(List<Double> data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'intakeData'");
+    }
+
+    @Override
+    public void clean() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clean'");
+    }
+
+    @Override
+    public void parse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+    }
+
+    @Override
+    public void sort() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+
+    @Override
+    public double maxTemperature() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'maxTemperature'");
+    }
+
+    @Override
+    public double minHumidity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'minHumidity'");
+    }
+    
+}
