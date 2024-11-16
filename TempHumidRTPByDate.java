8c486bc72d74d080e8e6a0e8ab5a5abc1c2648d1
ElliotScher <ecscher84@gmail.com>
2024-11-16 00:32:40 -0500

    fix

--- TempHumidRTPByDate.java
+++ TempHumidRTPByDate.java
@@ -1,19 +1,10 @@
 import java.util.LinkedList;
-import java.util.List;
 
 public class TempHumidRTPByDate extends TempHumidRTP {
     public double date;
 
     public TempHumidRTPByDate(double date) {
         this.date = date;
-    }
-
-    @Override
-    public void intakeData(List<Double> data) {
-        this.data = new LinkedList<>(data);
-        clean();
-        super.parse();
-        super.sort();
     }
 
     @Override
